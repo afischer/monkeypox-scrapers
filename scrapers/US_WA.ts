693934dcf7cc4f59ecf22fe41975c1af0bc61342
Andrew Fischer <andrew.fischer@nytimes.com>
2022-09-02 16:12:13 -0400

    Add washington, pull back to every 30m

---
+++ scrapers/US_WA.ts
@@ -0,0 +1,53 @@
+import axios from "axios";
+import cheerio from "cheerio";
+import { RegionType, ScrapeItem, Scraper } from "./scraper";
+import { getFIPSByCountyName } from "./util";
+
+export default class US_WA implements Scraper {
+  async run() {
+    const { data } = await axios.get(
+      "https://doh.wa.gov/you-and-your-family/illness-and-disease-z/monkeypox"
+    );
+
+    const $ = cheerio.load(data);
+
+    const entries: ScrapeItem[] = [];
+
+    $("table.tablesaw.tablesaw-stack tbody tr").each((i, el) => {
+      const data = $(el).find("td");
+      if (data.length !== 2) throw new Error("Unknown columns found");
+
+      let countyName = $(data[0]).text().trim();
+
+      const fips =
+        countyName.toLowerCase().startsWith("total") ? "53" : getFIPSByCountyName(countyName, "WA");
+
+      if (!countyName || !fips) {
+        throw new Error(`Unknown county ${countyName} fips ${fips}!`);
+      }
+
+      const cases = parseInt($(data[1]).text().replace(/,/g, ""));
+      if (!cases) throw new Error("Bad count!");
+
+      const region_type =
+        countyName === "Total"
+          ? RegionType.STATE
+          : RegionType.COUNTY;
+          
+      if (countyName.toLowerCase().startsWith("total")) {
+        countyName = "Washington";
+      }
+
+      entries.push({
+        country: "United States",
+        region_type,
+        source_name: "WA DOH",
+        region: countyName,
+        geoid: `US-${fips}`,
+        cases,
+      });
+    });
+
+    return entries;
+  }
+}
